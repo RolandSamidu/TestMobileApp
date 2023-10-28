9627b436460eeb3c3867891c8170aba61d749616
THARINDU-JAT <143626651+THARINDU-JAT@users.noreply.github.com>
2023-10-28 15:27:06 +0530

    Create Prescriptions Display Page

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/Home';
 import Signup from './src/Signup';
-import Login from './src/Login';
+//import Login from './src/Login';
+import PreList from './src/PreList';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,7 @@ function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="PreList" component={PreList} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/PreList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import Background from './Background';
+import Btn from './Btn';
+import { darkGreen, green } from './Constants';
+
+const PreList = (props) => {
+  return (
+    <Background>
+    
+    </Background>
+  );
+}
+
+export default PreList;
